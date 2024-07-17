972b40e9825583d47e9cf2fce8fb171d68b6a61f
Loskutova Marina <mloskutova1589@gmail.com>
2024-07-17 16:07:34 +0300

    first commit

---
+++ Interface/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Interface;
+
+namespace Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try 
+            {
+                Quadcopter quadcopter = new Quadcopter();
+                IRobot quadcopter2 = new Quadcopter();
+                IFlyingRobot quadcopter3 = new Quadcopter();
+                IChargeable quadcopter4 = new Quadcopter();
+
+                string? someComponents = string.Join(",", quadcopter.GetComponents());
+                Console.WriteLine(someComponents);
+
+                quadcopter.Charge();
+
+                Console.WriteLine(quadcopter2.GetRobotType());
+                Console.WriteLine(quadcopter3.GetRobotType());
+
+                Console.WriteLine(quadcopter2.GetInfo());
+                Console.WriteLine(quadcopter4.GetInfo());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Interface/Quadcopter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    public class Quadcopter : IFlyingRobot, IChargeable
+    {
+        private readonly List<string> _components = new List<string> { "rotor1", "rotor2", "rotor3", "rotor4" };
+
+        public List<string> GetComponents() 
+        {
+            return _components;
+        }
+        public void Charge() 
+        {
+            Console.WriteLine("Charging...");
+            Thread.Sleep(3000);
+            Console.WriteLine("Charged!");
+        }
+        string IChargeable.GetInfo() => "Info from IChargeable!";
+        string IRobot.GetInfo() => "Info from IRobot!";
+    }
+}
